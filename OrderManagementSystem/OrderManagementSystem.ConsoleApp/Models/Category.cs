f8c59c35afe8c8c9ad7cec855a20991835ea4361
Rodrigo Palazon <rpal@netcompany.com>
2026-04-06 17:50:34 +0200

    Add initial domain models

--- OrderManagementSystem/OrderManagementSystem.ConsoleApp/Models/Category.cs
+++ OrderManagementSystem/OrderManagementSystem.ConsoleApp/Models/Category.cs
@@ -8,5 +8,10 @@ namespace OrderManagementSystem.ConsoleApp.Models
 {
     internal class Category
     {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+
+        public string Description { get; set; } = string.Empty;
     }
 }
