5f42e41d8d094992007d639bcc0656201c553718
Rodrigo Palazon <rpal@netcompany.com>
2026-04-06 22:17:57 +0200

    Add repository interfaces for customer, category, and product

--- OrderManagementSystem/OrderManagementSystem.ConsoleApp/Interfaces/ICustomerRepository.cs
+++ OrderManagementSystem/OrderManagementSystem.ConsoleApp/Interfaces/ICustomerRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OrderManagementSystem.ConsoleApp.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,11 @@ namespace OrderManagementSystem.ConsoleApp.Interfaces
 {
     internal class ICustomerRepository
     {
+        List<Customer> GetAll();
+        Customer? GetById(int id);
+        Customer? GetByEmail(string email);
+        void Add(Customer customer);
+        void Update(Customer customer);
+        void Delete(int id);
     }
 }
