914713360cca14228842adb45b621c53bcdd9b95
Rodrigo Palazon <rpal@netcompany.com>
2026-04-20 09:36:02 +0200

    Clean mvc demo

--- OrderManagementSystem/OrderManagementSystem.ConsoleApp/Program.cs
+++ OrderManagementSystem/OrderManagementSystem.ConsoleApp/Program.cs
@@ -6,34 +6,56 @@ using OMS.Services.Services;
 
 namespace OMS.ConsoleApp
 {
-    public class Program
+    internal class Program
     {
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {
             ICategoryRepository categoryRepository = new CategoryRepository();
+            ICustomerRepository customerRepository = new CustomerRepository();
+            IProductRepository productRepository = new ProductRepository();
+
             ICategoryService categoryService = new CategoryService(categoryRepository);
+            ICustomerService customerService = new CustomerService(customerRepository);
+            IProductService productService = new ProductService(productRepository, categoryRepository);
 
-            var categoryMenu = new CategoryMenu(categoryService);
+            CategoryMenu categoryMenu = new CategoryMenu(categoryService);
+            CustomerMenu customerMenu = new CustomerMenu(customerService);
+            ProductMenu productMenu = new ProductMenu(productService);
 
-            while (true)
+            bool isRunning = true;
+
+            while (isRunning)
             {
                 Console.Clear();
                 Console.WriteLine("=== Order Management System ===");
                 Console.WriteLine("1. Manage Categories");
+                Console.WriteLine("2. Manage Customers");
+                Console.WriteLine("3. Manage Products");
                 Console.WriteLine("0. Exit");
                 Console.Write("Choose an option: ");
 
-                var option = Console.ReadLine();
+                string? option = Console.ReadLine();
 
                 switch (option)
                 {
                     case "1":
                         categoryMenu.Show();
                         break;
+
+                    case "2":
+                        customerMenu.Show();
+                        break;
+
+                    case "3":
+                        productMenu.Show();
+                        break;
+
                     case "0":
-                        return;
+                        isRunning = false;
+                        break;
+
                     default:
-                        Console.WriteLine("Invalid option.");
+                        Console.WriteLine("Invalid option. Press any key to continue...");
                         Console.ReadKey();
                         break;
                 }
